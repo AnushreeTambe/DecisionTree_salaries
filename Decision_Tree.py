7fb9619b62260df5aaee0d8137fc171e810ff8fb
AnushreeTambe <143435660+AnushreeTambe@users.noreply.github.com>
2024-02-06 07:34:02 +0400

    First commit

---
+++ Decision_Tree.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 09:30:49 2024
+
+@author: dell
+"""
+
+import pandas as pd
+df=pd.read_csv("C:/2-dataset/salaries.csv")
+df.head()
+inputs=df.drop('salary_more_then_100k',axis='columns')
+target=df['salary_more_then_100k']
+from sklearn.preprocessing import LabelEncoder
+le_company=LabelEncoder()
+le_job=LabelEncoder()
+le_degree=LabelEncoder()
+inputs['company_n'] =le_company.fit_transform(inputs['company'])
+inputs['job_n'] =le_job.fit_transform(inputs['job'])
+inputs['degree_n'] =le_degree.fit_transform(inputs['degree'])
+inputs_n=inputs.drop(['company','job','degree'],axis='columns')
+target
+
+from sklearn import tree
+model = tree.DecisionTreeClassifier() #To check the information : cntl+I
+model.fit(inputs_n, target)
+#Is salary of Google, Computer Engineer, Bachelors degree > 100k?
+model.predict([[2,1,0]])
+#Is salary of Google, Computer Engineer, Masters degree > 100k?
+model.predict([[2,1,1]])
